0466902fe4a614275e4ef4a4b0e00ee3a76fc947
Jose Alexis Hernandez Ricardo <jahernandezricardo@gmail.com>
2021-03-04 20:15:48 -0300

    removing freshclam schedule

--- alpine/main/bootstrap.sh
+++ alpine/main/bootstrap.sh
@@ -23,8 +23,8 @@ if [ ! -f ${MAIN_FILE} ]; then
     /usr/bin/freshclam
 fi
 
-echo "[bootstrap] Schedule freshclam DB updater."
-/usr/bin/freshclam -d -c 6
+#echo "[bootstrap] Schedule freshclam DB updater."
+#/usr/bin/freshclam -d -c 6
 
 echo "[bootstrap] Run clamav daemon..."
 exec /usr/sbin/clamd -c /etc/clamav/clamd.conf
